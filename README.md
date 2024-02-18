7148220dd48627ae8dd0ece0384cbf53881707cc
jero98772 <jero98772@gmail.com>
2024-02-17 20:32:53 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Glow-jobs
+
+# Job Post Search and Analysis
+
+This Python script is designed to search for job posts on a given website, analyze the content of each post, and save relevant information to a CSV file.
+
+## Requirements
+
+- Python 3.x
+- `requests` library
+- `BeautifulSoup` library
+- `urllib.parse` module
+
+## Installation
+
+1. Install Python 3.x from [python.org](https://www.python.org/downloads/).
+2. Install the required libraries using pip:
+   ```bash
+   pip install requests beautifulsoup4
+   ```
+
+## Usage
+
+1. Clone or download the repository.
+2. Navigate to the directory containing the script.
+3. Run the script with Python:
+   ```bash
+   python job_post_search.py
+   ```
+4. Follow the prompts to enter the URL of the website to search and the job title to look for.
+
+## Features
+
+- Searches for job posts on a given website.
+- Analyzes the content of each post to extract relevant information.
+- Saves the job title, company name, location, and URL to a TXT file.
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+
---
+++ libs.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+FILENAME="jobs.txt"
+
+def search_job_posts(url, job_title):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    job_posts = soup.find_all('div', class_='job-post')
+    for job_post in job_posts:
+        title = job_post.find('h2', class_='job-title').text.strip()
+        company = job_post.find('span', class_='company-name').text.strip()
+        location = job_post.find('span', class_='location').text.strip()
+        job_url = urljoin(url, job_post.find('a')['href'])
+    return [title,company,location,job_url]
+
+def write_txt(name,content,mode='a'):
+	"""
+	write_txt(name,content) , write in txt file something  
+	"""
+	content =str(content)
+	with open(name, mode) as file:
+		file.write(content)
+		file.close()
+
+def get_jobs(soup,url):
+    links = soup.find_all('a', href=True)
+    return [urljoin(url, link['href']) for link in links]
+
+def get_links(soup,url):
+    """
+    this function analize the webpage, searching inside the html code for <a> tags and get the argument of href and return a list with that, all links
+    """
+    links = soup.find_all('a', href=True)
+    return [urljoin(url, link['href']) for link in links]
+def scrapper(url):
+    """
+    this function get the webpage and apply 2 functions get_links(),get_jobs()
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    job_information=0#get_jobs(soup,url)
+    links = get_links(soup,url)
+    return links,job_information
+def crawl(url, depth=3):
+    visited = set()
+    queue = [(url, 0)]
+    while queue:
+        current_url, current_depth = queue.pop(0)
+        
+        if current_url in visited or current_depth >= depth:
+            continue
+        
+        visited.add(current_url)
+        print(f'Crawling {current_url} at depth {current_depth}')
+        links,job_information = scrapper(current_url)
+        for link in links:
+            queue.append((link, current_depth + 1))
+            write_txt(FILENAME,link+"\n")          
+
+crawl('https://example.com')
+
+"""
+pip install BeautifulSoup4
+"""
