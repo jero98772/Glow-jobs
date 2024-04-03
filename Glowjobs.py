5c199e6a2a6b93544a06270f683d9e6c4c96370c
dasjero98772 <jero98772@gmail.com>
2024-04-03 13:11:25 -0500

    update

--- Glowjobs.py
+++ Glowjobs.py
@@ -10,6 +10,25 @@ class webpage():
   @app.route("/offers")
   def offers():
     return render_template("offers.html")
+  @app.route("/blog")
+  def blog():
+    return render_template("blog.html")
+  @app.route("/launchapp")
+  def launchapp():
+    return render_template("launchapp.html")
+  @app.route("/conctact")
+  def conctact():
+    return render_template("conctact.html")
+  @app.route("/get_premium")
+  def get_premium():
+    return render_template("get_premium.html")
+
+      
+
+      
+
+      
+
       
 if __name__ == "__main__":
   app.run(debug=True,host="127.0.0.1",port=5000)
