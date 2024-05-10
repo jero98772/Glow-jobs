dd490677ba562e7ebdcc859b20a02b5326d657f9
dasjero98772 <jero98772@gmail.com>
2024-05-10 09:19:08 -0500

    update

--- tools/data_base.py
+++ tools/data_base.py
@@ -1,1 +1,15 @@
 import pymongo
+
+Host="mongodb://localhost:27017/"
+
+client = pymongo.MongoClient(Host)
+mydb = client["mydatabase"]
+
+mycol = mydb["customers"]
+
+# Insert a document into the "customers" collection
+mydict = { "name": "John", "address": "Highway 37" }
+x = mycol.insert_one(mydict)
+
+# Print the inserted document's ID
+print(x.inserted_id)
